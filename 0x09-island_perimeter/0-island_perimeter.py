c15eb9d4315032a45e3ca34ecf918c5a5bd17152
Saddamzy001 <saddamhusseini1234@gmail.com>
2023-09-03 00:36:37 -0700

    making updates

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,75 +1,25 @@
 #!/usr/bin/python3
-"""
-Defines function to calculate the perimeter of an island
-represented by a list of list of ints
+"""Island perimeter computing module.
 """
 
 
 def island_perimeter(grid):
+    """Computes the perimeter of an island with no lakes.
     """
-    Calculates the perimeter of an island represented by a list of list of ints
-
-    parameters:
-        grid [list of list of ints]: represents the island & surrounding water
-            * 0s represent water & 1s represent land
-            * Each cell is a sqaure with side length of 1
-            * Cells are connected horizontally/vertically (not diagonally)
-            * Grid is rectangular, with width and height not exceeding 100
-            * The grid is completely surrounded by water
-            * Islands do not have "lakes" (water completely inside the island)
-            * There is only one island or nothing
-
-    returns:
-        the perimeter of the island
-    """
-    error_msg = "grid must be a list of lists of ints representing an island"
-    if type(grid) is not list:
-        raise TypeError(error_msg)
-    for row in grid:
-        if type(row) is not list:
-            raise TypeError(error_msg)
-        for column in row:
-            if type(column) is not int:
-                raise TypeError(error_msg)
-            if column is not 0 and column is not 1:
-                raise ValueError(
-                    "grid must contain only 0s & 1s representing water & land")
     perimeter = 0
-    for row in range(len(grid)):
-        for column in range(len(grid[row])):
-            if grid[row][column] == 1:
-                perimeter += is_edge(grid, row, column)
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
-
-
-def is_edge(grid, row, column):
-    """
-    Determines if the given cell is on the edge of the island
-
-    parameters:
-        grid [list of lists of ints]: represents the island
-        row [int]: the index of row for the current cell
-        column [int]: the index of column for the current cell
-
-    returns:
-        1-4, if the given cell is on an edge, the number of edges
-        0, if the cell of land is in the interior of the island
-    """
-    edge_count = 0
-    if row == 0:
-        edge_count += 1
-    if row == (len(grid) - 1):
-        edge_count += 1
-    if row != 0 and grid[row - 1][column] == 0:
-        edge_count += 1
-    if row != (len(grid) - 1) and grid[row + 1][column] == 0:
-        edge_count += 1
-    if column == 0:
-        edge_count += 1
-    if column == (len(grid[row]) - 1):
-        edge_count += 1
-    if column != 0 and grid[row][column - 1] == 0:
-        edge_count += 1
-    if column != (len(grid[row]) - 1) and grid[row][column + 1] == 0:
-        edge_count += 1
-    return edge_count
--- 0x09-island_perimeter/README.md
+++ 0x09-island_perimeter/README.md
@@ -1,16 +1,36 @@
-# 0x1C-island_perimeter
-This interview algorithm project represents an island as a grid of integers. The list of list of integers contains 0s, that represent water, and 1s, that represent land.  Each cell in the grid is a square with side length of 1.  Cells are connected horizontally/vertically (not diagonally).  The grid itself is rectangular, with its width and height not exceeding 100.  The grid is completely surrounded by water.  There is either one island or nothing in the grid.  There are no "lakes" (water inside that isn't connected to the water surrounding the island).
+0x09. Island Perimeter
+======================
 
-[Island Perimeter](/0x1C-island_perimeter/0-island_perimeter.py)
-* Write a function in Python `def island_perimeter(grid)` that finds the perimeter of the island represented by the given grid:
-  * The grid is a list of list of integers:
-    * 0s represent water
-    * 1s represent land
-    * Each cell is a square with side length of 1
-    * Cells are connected horizontally/vertically (not diagonally)
-  * The grid is completely surrounded by water.
-  * There is only one island (or nothing).
-  * The island doesn't have water inside that isn't connected to the water surrounding the island.
+AlgorithmPython
 
-### test_files
-The [test_files](/0x1C-island_perimeter/test_files/) directory contains files used to test the output of the algorithm locally.
+-   By: Alexa Orrico, Software Engineer at Holberton School
+-   Weight: 1
+
+Requirements
+------------
+
+### General
+
+-   Allowed editors: `vi`, `vim`, `emacs`
+-   All your files will be interpreted/compiled on Ubuntu 14.04 LTS using `python3` (version 3.4.3)
+-   All your files should end with a new line
+-   The first line of all your files should be exactly `#!/usr/bin/python3`
+-   A `README.md` file, at the root of the folder of the project, is mandatory
+-   Your code should use the `PEP 8` style (version 1.7)
+-   You are not allowed to import any module
+-   All modules and functions must be documented
+-   All your files must be executable
+
+Tasks
+-----
+
++ [x] 0. **Island Perimeter**<br/>[0-island_perimeter.py](0-island_perimeter.py) contains a module with a function having the prototype `function def island_perimeter(grid):`, which returns the perimeter of the island described in `grid`, with the following requirements:
+  + `grid` is a list of list of integers:
+    + 0 represents water.
+    + 1 represents land.
+    + Each cell is square, with a side length of 1.
+    + Cells are connected horizontally/vertically (not diagonally).
+    + `grid` is rectangular, with its width and height not exceeding 100.
+  + The grid is completely surrounded by water.
+  + There is only one island (or nothing).
+  + The island doesn't have "lakes' (water inside that isn't connected to the water surrounding the island).
